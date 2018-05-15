918fbbdf0baf0516da334af3529a0550da75afbd
joaak <29533036+joaak@users.noreply.github.com>
2018-05-14 23:04:02 -0400

    Update basic_ann.py

--- Udemy Deep Learning Course/basic_ann.py
+++ Udemy Deep Learning Course/basic_ann.py
@@ -38,6 +38,7 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+
 # Part 2 - Making the ANN
 
 # Importing the Keras libraries and packages
@@ -78,3 +79,20 @@ from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
 # Test accuracy of 0.8425
+
+# Predicting a single new observation
+"""Predict if the customer with the following informations will leave the bank:
+Geography: France
+Credit Score: 600
+Gender: Male
+Age: 40
+Tenure: 3
+Balance: 60000
+Number of Products: 2
+Has Credit Card: Yes
+Is Active Member: Yes
+Estimated Salary: 50000"""
+new_prediction = classifier.predict(sc.transform(np.array([[0.0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])))
+new_prediction = (new_prediction > 0.5)
+
+# new_prediction = False
